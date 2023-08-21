a1310394427f171185f6a7d9612fbf07328c6619
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-08-21 17:33:16 -0500

    Experimentations in JavaScript type Coercion

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,22 @@
+document.write(typeof (5));
+document.write("10" + 5);
+document.write(0 / 0 + ` | `);
+document.write(`is 15 NOT a number? ` + isNaN(15) + ` | `);
+document.write(`is "string" NOT a number? ` + isNaN("string") + ` | `);
+document.write(3E310 + ` | `);
+document.write(-3E310 + ` | `);
+document.write(`is 10 greater than 5? ` + (10 > 5) + ` | `);
+console.log(2 + 2);
+console.log(10 > 50);
+console.log(10 == 10);
+console.log(10 == 12);
+
+console.log(10 === 10);
+console.log(10 === "10");
+console.log(10 === 5);
+console.log("10" === 5);
+
+console.log("hello" == "hello" && "hello" == "hello");
+console.log("hello" == "hello" || "hlo" == "he");
+
+console.log(25 != 50);
