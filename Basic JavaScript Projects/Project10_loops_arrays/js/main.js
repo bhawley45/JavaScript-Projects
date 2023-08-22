17247f1f7c4d03ef30960fcd21babb36b90a4dcb
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-08-21 22:25:52 -0500

    Finishing for the day, New projects have to do with loops, arrays, and objects

---
+++ Basic JavaScript Projects/Project10_loops_arrays/js/main.js
@@ -0,0 +1,48 @@
+function Call_Loop() {
+    let x = 0;
+    while (x <= 10) {
+        console.log(x); // Print the value of x to the console
+        x++;
+    }
+}
+
+
+// Instrument list section
+
+var _Instruments = ["Guitar", "Piano", "Violin"];
+var sortedInstruments = ``;
+
+function forLoop() {
+    for (let i = 0; i < _Instruments.length; i++) {
+        sortedInstruments += _Instruments[i] + `<br>`;
+    }
+    document.getElementById(`List_of_Instruments`).innerHTML = sortedInstruments;
+}
+
+var _Dogs = ["Poodle", "Labrador", "PitBull", "Retriever"];
+var sortedDogs = ``;
+
+function arrayFunction() {
+    for (let i = 0; i < _Dogs.length; i++) {
+        sortedDogs += _Dogs[i] + `<br>`;
+    }
+    document.getElementById(`Array`).innerHTML = sortedDogs;
+}
+
+
+const myObject = {
+    name: "John",
+    age: 30,
+    occupation: "Developer"
+};
+
+// Change property value
+myObject.age = 31;
+
+// Add a new property
+myObject.location = "New York";
+
+function constantFunction() {
+    const output = `Name: ${myObject.name}, Age: ${myObject.age}, Occupation: ${myObject.occupation}, Location: ${myObject.location}`;
+    document.getElementById("Constant").innerHTML = output;
+}
